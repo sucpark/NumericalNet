614b5dae7c39f8058492c97ae77f4952be5438d7
sucpark <sucpark0317@gmail.com>
2024-03-31 22:37:17 +0900

    Update minor issue

--- model.py
+++ model.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch import Tensor, nn
-
 
 class NumericOperationDataset(torch.utils.data.Dataset):
     def __init__(self, data, unit_to_idx, operation_to_idx):
