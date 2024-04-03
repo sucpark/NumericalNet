caa69592f28e8d920574248644099380b72481b9
sucpark <sucpark0317@gmail.com>
2024-04-03 22:10:34 +0900

    Update net structure and loss_fn

--- model.py
+++ model.py
@@ -22,61 +22,91 @@ class NumericOperationDataset(torch.utils.data.Dataset):
         return value1, unit1, value2, unit2, operation
     
 class NumericalNet(nn.Module):
-    def __init__(self, unit_to_idx, operation_to_idx, device, use_bias=True):
+    def __init__(self, unit_to_idx, operation_to_idx, device, dim=200, use_bias=False):
         super(NumericalNet, self).__init__()
         
         self.unit_to_idx = unit_to_idx
         self.operation_to_idx = operation_to_idx
         self.device = device
-        self.fc1 = nn.Linear(in_features=1, out_features=256, bias=use_bias)
-        self.ln1 = nn.LayerNorm(256)
-        self.fc2 = nn.Linear(in_features=256, out_features=512, bias=use_bias)
-        self.ln2 = nn.LayerNorm(512)
-        self.fc3 = nn.Linear(in_features=512, out_features=1024, bias=use_bias)
-        self.ln3 = nn.LayerNorm(1024)
+        self.unit_embeddings = nn.Embedding(len(self.unit_to_idx), dim)
         
-        self.unit_embeddings = nn.Embedding(len(self.unit_to_idx), 1024)
-        self.joint_fc1 = nn.Linear(in_features=2*1024, out_features=1024, bias=use_bias)
-        self.ln4 = nn.LayerNorm(1024)
-        self.crossnet = CrossNet(in_features=2*1024, parameterization='matrix')
+        self.digit_fc1 = nn.Linear(in_features=1, out_features=dim, bias=use_bias)
+        self.digit_fc2 = nn.Linear(in_features=dim, out_features=dim, bias=use_bias)
+        self.digit_fc3 = nn.Linear(in_features=dim, out_features=dim, bias=use_bias)
+        
+        self.unit_fc1 = nn.Linear(in_features=dim, out_features=dim, bias=use_bias)
+        self.unit_fc2 = nn.Linear(in_features=dim, out_features=dim, bias=use_bias)
+        
+        self.joint_fc1 = nn.Linear(in_features=dim, out_features=dim, bias=use_bias)
+        self.joint_fc2 = nn.Linear(in_features=dim, out_features=dim, bias=use_bias)
         
     def forward(self, x):
-        x = F.leaky_relu(self.ln1(self.fc1(x)))
-        x = F.leaky_relu(self.ln2(self.fc2(x)))
-        x = self.ln3(self.fc3(x))
+        x = self.digit_fc1(x)
+        x = F.leaky_relu(x)
+        x = self.digit_fc2(x)
+        x = F.leaky_relu(x)
+        x = self.digit_fc3(x)
         return x
     
+    def get_digit_embedding(self, digit):
+        digit = torch.tensor([float(digit)], dtype=torch.float32).to(self.device)
+        digit_embedding = self.forward(digit)
+        return digit_embedding
+    
     def get_unit_embedding(self, unit):
-        unit_embedding = self.unit_embeddings(unit)
-        unit_embedding = unit_embedding.squeeze()
-        return unit_embedding
+        x = self.unit_embeddings(unit).squeeze()
+        x = self.unit_fc1(x)
+        x = F.leaky_relu(x)
+        x = self.unit_fc2(x)
+        return x
 
     def joint_embedding(self, value_embedding, unit_embedding):       
-        joint_emb = torch.cat((value_embedding, unit_embedding), -1)
-        joint_emb = self.crossnet(joint_emb)
-        joint_emb = self.joint_fc1(joint_emb)
-        return joint_emb
+        x = value_embedding + unit_embedding
+        x = self.joint_fc1(x)
+        x = F.leaky_relu(x)
+        x = self.joint_fc2(x)
+        return x
     
     def get_digit_unit_embedding(self, digit, unit):
-        digit = float(digit)
-        digit = torch.tensor([digit], dtype=torch.float32).to(self.device)
         unit = torch.tensor([self.unit_to_idx[unit]]).to(self.device)
-        
-        digit_embedding = self.forward(digit).view(1,-1)
+        digit_embedding = self.get_digit_embedding(digit).view(1,-1)
         unit_embedding = self.get_unit_embedding(unit).view(1,-1)
         
         return self.joint_embedding(digit_embedding, unit_embedding)
 
-    def loss_fn(self, value1_embedding, value2_embedding, unit1, unit2, alpha=2, beta=1.0):
+    def loss_fn(self, value1_embedding, value2_embedding, unit1, unit2, operation, alpha=1.5, beta=0.8):
         unit1_embedding = self.get_unit_embedding(unit1)
         unit2_embedding = self.get_unit_embedding(unit2)
-
         value1_unit1_embedding = self.joint_embedding(value1_embedding, unit1_embedding)
         value2_unit2_embedding = self.joint_embedding(value2_embedding, unit2_embedding)
-
-        equality_loss = torch.mean(torch.abs(value1_unit1_embedding - value2_unit2_embedding))
-        unit_loss = F.mse_loss(unit1_embedding, unit2_embedding)
         
-        output_loss = alpha * equality_loss + beta * unit_loss
+        equality_mask = (operation == self.operation_to_idx['='])
+        less_than_mask = (operation == self.operation_to_idx['<'])
+        greater_than_mask = (operation == self.operation_to_idx['>'])
+
+        # equality_loss = torch.mean(torch.masked_select(torch.abs(value1_unit1_embedding - value2_unit2_embedding), equality_mask.unsqueeze(-1)))
+        # equality_loss = torch.mean(torch.masked_select((value1_unit1_embedding - value2_unit2_embedding)**2, equality_mask.unsqueeze(-1)))
+    
+        value1_unit1_norm = torch.norm(value1_unit1_embedding, p=2, dim=1)
+        value2_unit2_norm = torch.norm(value2_unit2_embedding, p=2, dim=1)
+
+        equality_loss = torch.mean(torch.masked_select((value1_unit1_norm - value2_unit2_norm)**2, equality_mask))
+
+        less_than_target = less_than_mask.float().squeeze()
+        greater_than_target = greater_than_mask.float().squeeze()
+
+        less_than_pred = (value1_unit1_norm < value2_unit2_norm).float()
+        greater_than_pred = (value1_unit1_norm > value2_unit2_norm).float()
+        
+        less_than_loss = F.binary_cross_entropy(less_than_pred, less_than_target)
+        greater_than_loss = F.binary_cross_entropy(greater_than_pred, greater_than_target)
+
+        inequality_loss = torch.mean(less_than_loss * less_than_mask.float() + greater_than_loss * greater_than_mask.float())
+        output_loss = alpha*equality_loss + beta*inequality_loss
 
         return output_loss
+    
+    
+
+    
+    
