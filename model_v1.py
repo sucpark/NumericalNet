2a30bd2614cadfc8eab5a8d9aea2c90b54f39f54
sucpark <sucpark0317@gmail.com>
2024-04-08 16:34:25 +0900

    Update code: generate ver2.0

--- model_v1.py
+++ model_v1.py
@@ -25,17 +25,13 @@ class NumericOperationDataset(torch.utils.data.Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        value1, unit1, value2, unit2, diff_value, diff_unit, operation, equal = self.data[idx]
+        value1, unit1, value2, unit2, operation = self.data[idx]
         value1 = torch.tensor([value1], dtype=torch.float32)
         value2 = torch.tensor([value2], dtype=torch.float32)
         unit1 = torch.tensor([self.unit_to_idx[unit1]], dtype=torch.long)
         unit2 = torch.tensor([self.unit_to_idx[unit2]], dtype=torch.long)
         operation = torch.tensor([self.operation_to_idx[operation]], dtype=torch.long)
-        diff_value = torch.tensor([diff_value], dtype=torch.float32)
-        diff_unit = torch.tensor([self.unit_to_idx[diff_unit]], dtype=torch.long)
-        equal = torch.tensor([equal], dtype=torch.int32)
-        
-        return value1, unit1, value2, unit2, diff_value, diff_unit, operation, equal
+        return value1, unit1, value2, unit2, operation
     
 class NumericalNet(nn.Module):
     def __init__(self, unit_to_idx, operation_to_idx, device, dim=200, num_layers=2, use_bias=True):
@@ -53,8 +49,8 @@ class NumericalNet(nn.Module):
         self.digit_layers.append(nn.Linear(in_features=1, out_features=dim, bias=use_bias))
         for idx in range(num_layers):
             self.digit_layers.append(nn.Linear(in_features=dim, out_features=dim, bias=use_bias))
-            # if idx % 2 == 0:
-            #     self.digit_layers.append(nn.LayerNorm(dim))
+            if idx % 2 == 0:
+                self.digit_layers.append(nn.LayerNorm(dim))
         self.digit_layers.append(nn.Linear(in_features=dim, out_features=dim, bias=use_bias))
 
         # Build unit layers
@@ -62,30 +58,29 @@ class NumericalNet(nn.Module):
         self.unit_layers.append(nn.Linear(in_features=dim, out_features=dim, bias=use_bias))
         for idx in range(num_layers):
             self.unit_layers.append(nn.Linear(in_features=dim, out_features=dim, bias=use_bias))
-            # if idx % 2 == 0:
-            #     self.unit_layers.append(nn.LayerNorm(dim))
+            if idx % 2 == 0:
+                self.unit_layers.append(nn.LayerNorm(dim))
         self.unit_layers.append(nn.Linear(in_features=dim, out_features=dim, bias=use_bias))
 
         # Build joint layers
         self.joint_layers = nn.ModuleList()
-        self.joint_layers.append(nn.Linear(in_features=dim*2, out_features=dim, bias=use_bias))
+        self.joint_layers.append(nn.Linear(in_features=dim, out_features=dim, bias=use_bias))
         for idx in range(num_layers):
             self.joint_layers.append(nn.Linear(in_features=dim, out_features=dim, bias=use_bias))
-            # if idx % 2 == 0:
-            #     self.joint_layers.append(nn.LayerNorm(dim))
+            if idx % 2 == 0:
+                self.joint_layers.append(nn.LayerNorm(dim))
         self.joint_layers.append(nn.Linear(in_features=dim, out_features=dim, bias=use_bias))
 
     def forward(self, digit, unit):
         digit_embedding = self.get_digit_embedding(digit)
         unit_embedding = self.get_unit_embedding(unit)
-        digit_unit = torch.concatenate([digit_embedding, unit_embedding], dim=-1)
-        # digit_unit = digit_embedding + unit_embedding
+        # digit_unit = torch.concatenate([digit_embedding, unit_embedding], dim=-1)
+        digit_unit = digit_embedding + unit_embedding
         for layer in self.joint_layers:
             digit_unit = layer(digit_unit)
         return digit_unit
 
     def get_digit_embedding(self, digit):
-        digit = torch.log(digit+1e-9)
         if not torch.is_tensor(digit):
             digit = torch.tensor([float(digit)], dtype=torch.float32).to(self.device)
         digit_embedding = digit
@@ -104,50 +99,21 @@ class NumericalNet(nn.Module):
             unit_embedding = layer(unit_embedding)
         return unit_embedding 
     
-    def loss_fn(self, embedding1, embedding2, embedding3, operations, equal, alpha=1.2, beta=0.8):
+    def loss_fn(self, embedding1, embedding2, operations, alpha=1.2, beta=0.8):
 
-        equality_mask = (equal == 1)
-        addition_mask = (equal == 0) & (operations == self.operation_to_idx['+'])
-        subtraction_mask = (equal == 0) & (operations == self.operation_to_idx['-'])
+        equality_mask = (operations == self.operation_to_idx['='])
+        less_than_mask = (operations == self.operation_to_idx['<'])
+        greater_than_mask = (operations == self.operation_to_idx['>'])
         
-        embedding1_l2norm = torch.norm(embedding1, p=2, dim=-1)
-        embedding2_l2norm = torch.norm(embedding2, p=2, dim=-1)
-        embedding3_l2norm = torch.norm(embedding3, p=2, dim=-1)
+        # Compute equality loss
+        equality_loss = F.mse_loss(torch.masked_select(embedding1, equality_mask), torch.masked_select(embedding2, equality_mask))
         
-        equality_mse_loss = F.mse_loss(
-            torch.masked_select(embedding1, equality_mask),
-            torch.masked_select(embedding3, equality_mask)
-        )
+        # Compute comparison loss
+        norm1, norm2 = torch.norm(embedding1, p=2, dim=1), torch.norm(embedding2, p=2, dim=1)
+        less_than_loss = torch.sum(torch.max(torch.zeros_like(norm1), norm1 - norm2) * less_than_mask) / torch.sum(less_than_mask)
+        greater_than_loss = torch.sum(torch.max(torch.zeros_like(norm1), norm2 - norm1) * greater_than_mask) / torch.sum(greater_than_mask)
+        # comparison_loss = torch.log(less_than_loss + greater_than_loss + 1)
+        comparison_loss = (less_than_loss + greater_than_loss)*0.5
 
-        addition_mse_loss = F.mse_loss(
-            torch.masked_select(embedding1, addition_mask) + torch.masked_select(embedding2, addition_mask), 
-            torch.masked_select(embedding3, addition_mask)
-        )
-        
-        subtraction_mse_loss = F.mse_loss(
-            torch.masked_select(embedding1, subtraction_mask) - torch.masked_select(embedding2, subtraction_mask), 
-            torch.masked_select(embedding3, subtraction_mask)
-        )
-        
-        mse_loss = equality_mse_loss + addition_mse_loss + subtraction_mse_loss
-        
-        # equality_norm_loss = torch.mean((
-        #     torch.masked_select(embedding1_l2norm, equality_mask) - torch.masked_select(embedding3_l2norm, equality_mask)
-        #     )**2
-        # )
-        
-        # addition_norm_loss = torch.mean((
-        #     torch.masked_select(embedding1_l2norm, addition_mask) + torch.masked_select(embedding2_l2norm, addition_mask) - torch.masked_select(embedding3_l2norm, addition_mask)
-        #     )**2
-        # )
-        
-        # subtraction_norm_loss = torch.mean((
-        #     torch.masked_select(embedding1_l2norm, subtraction_mask) - torch.masked_select(embedding2_l2norm, subtraction_mask) - torch.masked_select(embedding3_l2norm, subtraction_mask)
-        #     )**2
-        # )
-        
-        # norm_loss = equality_norm_loss + addition_norm_loss + subtraction_norm_loss
-        
-        total_loss = alpha*mse_loss # + beta*norm_loss
-        
+        total_loss = alpha*equality_loss + beta*comparison_loss
         return total_loss
